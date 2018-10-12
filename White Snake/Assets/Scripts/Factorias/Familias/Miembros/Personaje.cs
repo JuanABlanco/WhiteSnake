a3510ff8e05d4ccfff455944aa45e024f39faf3f
Juan Blanco <juan15597l@gmail.com>
2018-10-11 23:27:07 -0400

    Agregados los atributos de personaje

---
+++ White Snake/Assets/Scripts/Factorias/Familias/Miembros/Personaje.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personaje : MonoBehaviour {
+
+    public int maxLife;
+    public int currentLife;
+    public float maxSpeed;
+    public float currentSpeed;
+    private string pName;
+    public List<Objeto> invetario;
+    public List<Objeto> drop;
+    public int baseDamage;
+    public int currentDamage;
+
+    public void Move() { }
+
+    public void Spawn() { }
+
+    public void Die() { }
+
+
+
+
+    
+}
