b970638a493ba09909b3712034dee211adf1b0e6
Juan Blanco <juan15597l@gmail.com>
2018-10-21 22:49:28 -0400

    Mensaje de NPC habilitado y Script de Dio listo

---
+++ White Snake/Assets/Scripts/Factorias/Familias/Miembros/DioController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DioController : NPCControler {
+
+    public DioController sharedInstance;
+
+    void Awake()
+    {
+        this.sharedInstance = this;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Player")
+        {
+            Say("Cuidate el dulce mas adelante...");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            Say(null);
+        }
+    }
+}
---
+++ White Snake/Assets/Scripts/Factorias/Familias/Miembros/NPCControler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPCControler : Personaje {
+    public Text ConBar;
+
+	void Awake()
+    {
+        this.maxLife = 10000;
+        this.currentLife = this.maxLife;
+    }
+
+    //Mostrar mensaje personalizado en la barra de conversaciones de la camara
+    protected void Say(string texto)
+    {
+        if (texto != null)
+        {
+            this.ConBar.text = this.name + ": " + texto;
+        }else
+        {
+            this.ConBar.text = "";
+        }
+    }
+
+
+}
