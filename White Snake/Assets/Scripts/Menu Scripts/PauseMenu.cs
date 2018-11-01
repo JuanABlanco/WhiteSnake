514ddf6096664c1385f1bc5cc699ecbfd4c27f7c
Juan Blanco <juan15597l@gmail.com>
2018-11-01 02:03:51 -0400

    Menus Implementados

---
+++ White Snake/Assets/Scripts/Menu Scripts/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool gameIsPaused = false;
+
+    /*
+     * Nombre del metodo: Pause
+     * Funcion: Pausa el tiempo del juego
+     * Parametros: Ninguno 
+     */
+
+    public void Pause()
+    {
+        gameIsPaused = true;
+        Time.timeScale = 0f;
+    }
+
+    /*
+     * Nombre del metodo: Resume
+     * Funcion: Reanuda el tiempo del juego
+     * Parametros: Ninguno 
+     */
+
+    public void Resume()
+    {
+        gameIsPaused = false;
+        Time.timeScale = 1f;
+    }
+
+    /*
+     * Nombre del metodo: MainMenu
+     * Funcion: Regresa al jugador al menu principal
+     * Parametros: Ninguno 
+     */
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
+
